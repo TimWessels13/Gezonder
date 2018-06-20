c1ae75826611f3bac85cf8bda3f25077a17895da
Tim Wessels <wessels.t@hetnet.nl>
2018-06-20 14:14:32 +0200

    ghngfhgfh

--- Gezonder/klanten.cs
+++ Gezonder/klanten.cs
@@ -8,12 +8,22 @@ namespace Gezonder
 {
     class Klanten
     {
+        public Klanten(string KlantNaam, string KlantAdres, string KlantPostcode, string KlantPlaats, int ArtsId, int MedicatieId, int VerzekeraarId)
+        {
+        }
+
         public string KlantNaam { get; set; }
         public string KlantAdres { get; set; }
         public string KlantPostcode { get; set; }
         public string KlantPlaats { get; set; }
-        public string ArtsNaam { get; set; }
-        public string MedicatieNaam { get; set; }
-        public string VerzekeraarNaam { get; set; }
+        public int ArtsId { get; set; }
+        public int MedicatieId { get; set; }
+        public int VerzekeraarId { get; set; }
+
+        public static void Main(string[] args)
+        {
+            List<Klanten> K = new List<Klanten>();
+            K.Add(new Klanten("Pietje Bel", "Pesterstraat 13", "1873 CS", "Veenendaal", 1, 1, 1));
+        }
     }
 }
